13a76e6e6b25262e514bfd225f6eb16fede52218
Adam Wulf <adam.wulf@gmail.com>
2016-06-18 21:57:42 -0400

    Added OSX framework

---
+++ TyphoonOSX/TyphoonOSX.h
@@ -0,0 +1,19 @@
+//
+//  TyphoonOSX.h
+//  TyphoonOSX
+//
+//  Created by Adam Wulf on 6/18/16.
+//  Copyright © 2016 typhoonframework.org. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TyphoonOSX.
+FOUNDATION_EXPORT double TyphoonOSXVersionNumber;
+
+//! Project version string for TyphoonOSX.
+FOUNDATION_EXPORT const unsigned char TyphoonOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TyphoonOSX/PublicHeader.h>
+
+
